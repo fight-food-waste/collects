7d82cd16b70eaede69ca02b1a4c2b0f554cc580b
Stéphane <rabena.stephane@gmail.com>
2019-05-19 15:47:53 +0200

    Add warehouses and shelves tables

---
+++ app/Shelf.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Shelf extends Model
+{
+    protected $fillable = ['number', 'warehouse_id'];
+
+    public function warehouse()
+    {
+        return $this->belongsTo(Warehouse::class);
+    }
+}
---
+++ database/migrations/07_00_00_00_create_shelves_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShelvesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shelves', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('number', 255);
+            $table->unsignedInteger('warehouse_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shelves');
+    }
+}
