591472db9efed9688a15915310aa5de1f13b40a4
Stéphane <rabena.stephane@gmail.com>
2019-08-11 12:16:48 +0200

    Translate 'dashboard' in home view

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'dashboard' => 'Dashboard',
     'message' => 'You are logged in!',
     'goto' => 'Go to the',
     'admin_panel' => 'administration panel',
--- resources/lang/fr/home.php
+++ resources/lang/fr/home.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'dashboard' => 'Tableau de bord',
     'message' => "Vous êtes connecté !",
     'goto' => 'Se rendre dans le',
     'admin_panel' => 'back office',
