bdec4b0bd60632061d005f6a661cce03fc113dad
angristan <angristan@pm.me>
2019-08-24 01:12:10 +0200

    Update dropdown order

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -105,19 +105,19 @@
                                     {{ __('main.account') }}
                                 </a>
 
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('main.logout') }}
-                                </a>
-
-
                                     @foreach (Config::get('languages') as $locale => $language)
                                         @if ($locale != App::getLocale())
                                             <a class="dropdown-item"
                                                href="{{ route('lang.switch', $locale) }}">{{$language}}</a>
                                         @endif
                                     @endforeach
+
+
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('main.logout') }}
+                                </a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST"
                                       style="display: none;">
